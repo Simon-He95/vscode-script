876c2469b921484f82effcc89060b378ee559c26
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-06-11 17:52:41 +0800

    chore: 用webview改造treeProvider

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,82 @@
 import * as vscode from 'vscode'
 import { watch } from 'chokidar'
-import { ScriptProvider } from './scriptModel'
+import { message, openFile } from '@vscode-use/utils'
+import { getwebviewScript } from '../media/webview'
+import { getwebviewHtml } from '../media/webviewHtml'
 import { readMakefile } from './common'
+import { webviewProvider } from './webviewProvider'
+import { getScripts, transformScriptToTreeData } from './utils'
 
-// todo:修改成webview 保留当前终端是否启动的状态
 export async function activate(context: vscode.ExtensionContext) {
   const workspaceFolders = vscode.workspace.workspaceFolders
   if (!workspaceFolders)
     return
   const { auth = '' } = vscode.workspace.getConfiguration('vscode-script')
   const projectPath = workspaceFolders[0].uri.fsPath
-  const todoDataProvider = new ScriptProvider(context, projectPath)
-
-  context.subscriptions.push(vscode.window.registerTreeDataProvider('vscode-scripts.id', todoDataProvider))
-
-  context.subscriptions.push(vscode.commands.registerCommand('vscode-scripts.switch', () => vscode.commands.executeCommand('workbench.view.extension.scripts')))
-
-  context.subscriptions.push(vscode.commands.registerCommand('vscode-scripts.run', async (...args) => {
-    runTerminal(args)
-  }))
-
-  context.subscriptions.push(vscode.commands.registerCommand('vscode-scripts.runMakefile', async (filepath: string) => {
-    // 读取Makefile文件内容提取所有的命令, 用gum来选择
-    const makefilePath = `${filepath}/Makefile`
-    const commands = await readMakefile(makefilePath)
-    if (!commands.length)
-      return vscode.window.showInformationMessage(`未能在${makefilePath}中找到可以执行的命令`)
-    // 我电脑没有权限暂时使用sudo make来启动
-    const runCommand = `cd ${filepath} && choose=$(echo "${commands.join('\\n')}" | gum filter --placeholder=" 请选择一个命令") && ${auth} make $choose || echo "已取消"`.replace(/\s+/g, ' ')
-    // 新开终端执行
-    const terminal = vscode.window.createTerminal()
-    terminal.show()
-    // 等待终端初始化完成输出指令
-    terminal.processId.then(() => setTimeout(() => terminal.sendText(runCommand), 800))
-  }))
-
-  // context.subscriptions.push(vscode.commands.registerCommand('vscode-scripts.runDebug', async (item: any) => {
-  //   const { command } = item
-  //   if (!command)
-  //     return
-  //   runTerminal(command.arguments, 'JavaScript Debug Terminal')
-  // }))
-
-  context.subscriptions.push(vscode.commands.registerCommand('vscode-scripts.view', ({ view }: any) => {
-    if (!view)
-      return
-    vscode.workspace.openTextDocument(view).then(
-      doc => vscode.window.showTextDocument(doc))
-  }))
+  const scripts = await getScripts(projectPath)
+  let treeData = transformScriptToTreeData(scripts)
   const CREATED_TERMINAL: any[] = []
+
+  const provider = webviewProvider(
+    context, {
+      treeData,
+    },
+    async (data) => {
+      const { type, value } = data
+      const _value = JSON.parse(value)
+      if (type === 'run') {
+        if (_value.absolutePath) {
+          const makefilePath = `${_value.absolutePath}/Makefile`
+          const commands = await readMakefile(makefilePath)
+          if (!commands.length) {
+            message(`未能在${makefilePath}中找到可以执行的命令`)
+            return
+          }
+          // 我电脑没有权限暂时使用sudo make来启动
+          const runCommand = `cd ${_value.absolutePath} && choose=$(echo "${commands.join('\\n')}" | gum filter --placeholder=" 请选择一个命令") && ${auth} make $choose || echo "已取消"`.replace(/\s+/g, ' ')
+          // 是否已经创建过次终端
+          const hasCreatedTerminal = CREATED_TERMINAL.find((_terminal) => {
+            const { projectPath, script } = _terminal
+            return projectPath === makefilePath && script === runCommand
+          })
+
+          if (hasCreatedTerminal) {
+            // 如果创建了就激活当前终端再次执行命令，而不重新打开新的
+            const targetTerminal = hasCreatedTerminal.terminal
+            targetTerminal.sendText(runCommand)
+            // 聚焦到此终端
+            targetTerminal.show()
+            return
+          }
+          // 新开终端执行
+          const terminal = vscode.window.createTerminal(_value.label)
+          const id = await terminal.processId!
+
+          CREATED_TERMINAL.push({
+            id,
+            projectPath: makefilePath,
+            script: runCommand,
+            terminal,
+          })
+          terminal.show()
+          // 等待终端初始化完成输出指令
+          terminal.processId.then(() => setTimeout(() => terminal.sendText(runCommand), 800))
+        }
+        else {
+          runTerminal(_value)
+        }
+      }
+      else if (type === 'view') {
+        const absolutePath = _value.absolutePath
+          ? `${_value.absolutePath}/Makefile`
+          : `${projectPath}/${_value.relativePath}`
+        openFile(absolutePath)
+      }
+    },
+  )
+
   async function runTerminal(args: any) {
-    const [script, env, workspaceName, _projectPath] = args
+    const { label: script, env, workspaceName, projectPath: _projectPath } = args
     let runCommand = ''
     if (projectPath !== _projectPath)
       runCommand += `cd ${_projectPath} && `
@@ -109,6 +135,7 @@ export async function activate(context: vscode.ExtensionContext) {
     // 等待终端初始化完成输出指令
     terminal.processId.then(() => setTimeout(() => terminal.sendText(runCommand), 800))
   }
+
   context.subscriptions.push(vscode.window.onDidCloseTerminal(async (terminal) => {
     const id = await terminal.processId
     if (!id)
@@ -127,8 +154,14 @@ export async function activate(context: vscode.ExtensionContext) {
       pollInterval: 100,
     },
   })
-  watcher.on('change', () => {
-    todoDataProvider.refresh()
+
+  watcher.on('change', async () => {
+    const scripts = await getScripts(projectPath)
+    treeData = transformScriptToTreeData(scripts)
+    provider.deferScript(getwebviewScript({
+      treeData,
+    }))
+    provider.refresh(getwebviewHtml())
   })
 }
 
